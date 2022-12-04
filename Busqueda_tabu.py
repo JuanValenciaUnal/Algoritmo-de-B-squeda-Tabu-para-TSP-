928d224448e098ca30a38324883b9782dd1a1702
JuanValenciaUnal <juanjose.valencia22@gmail.com>
2022-12-03 22:17:09 -0500

    Agregada funncion para identificar si un camino es tabu o no

--- Busqueda_tabu.py
+++ Busqueda_tabu.py
@@ -17,24 +17,39 @@ def calcular_peso(camino,pesos):
         peso=peso + pesos[camino[i]-1][camino[i+1]-1]
         print(peso," ",camino[i]-1," ",camino[i] )
     peso= peso + pesos[camino[-1]-1][camino[0]-1]
-    return peso    
+    return peso   
+
+def estabu(lista,lista_tabu):
+    band =0
+    for i in range(len(lista_tabu)):
+        
+        if(lista_tabu[i][0] in lista):
+            if(lista.index(lista_tabu[i][0]) != len(lista)-1):
+                if (lista[lista.index(lista_tabu[i][0])+1] == lista_tabu[i][1]):
+                      band =1
+    if( band == 1):
+        return True
+    else:
+        return False
+                
 
 def vecinos(camino,lista_tabu):
     vecinos = []
     for i in range(len(camino)):
         nuevo=camino.copy()
         nuevo[i-1],nuevo[i] = nuevo[i],nuevo[i-1]
-        vecinos.append(nuevo)
+        if(estabu(nuevo,lista_tabu) == False):
+            vecinos.append(nuevo)
     return vecinos        
         
 pesos = [[0,2,3,4,1],[1,0,4,1,2],[2,3,0,2,3],[3,4,1,0,4],[4,1,2,3,0]]
-lista_tabu= []
+lista_tabu= [[3, 4],[1, 2]]
 tenencia_tabu=3
 criterio_aspiracion = 100000
 
 
 #camino=incializar(pesos)
-camino=[1,2,3,4,5]
+camino=[2,1,4,3,5]
 #print(camino)
 #print(calcular_peso(camino, pesos))
 print(vecinos(camino, lista_tabu))
