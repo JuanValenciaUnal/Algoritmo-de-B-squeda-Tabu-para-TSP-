7015f76d1fd5acf816cc9db53b1da585c8472fb9
JuanValenciaUnal <juanjose.valencia22@gmail.com>
2022-12-04 00:49:43 -0500

    Funcion tabu completada y funcional para el paper elegido falta confirmar con otros ejereccios

--- Busqueda_tabu.py
+++ Busqueda_tabu.py
@@ -21,7 +21,7 @@ def calcular_peso(camino,pesos):
 def estabu(lista,lista_tabu):
     band =0
     for i in range(len(lista_tabu)):
-        
+        #print(" Esta es la lista",lista_tabu)
         if(lista_tabu[i][0] in lista):
             if(lista.index(lista_tabu[i][0]) != len(lista)-1):
                 if (lista[lista.index(lista_tabu[i][0])+1] == lista_tabu[i][1]):
@@ -39,6 +39,9 @@ def vecinos(camino,lista_tabu):
         nuevo[i-1],nuevo[i] = nuevo[i],nuevo[i-1]
         if(estabu(nuevo,lista_tabu) == False):
             vecinos.append(nuevo)
+    vecino_f=vecinos[0]
+    vecinos.remove(vecinos[0])
+    vecinos.append(vecino_f)
     return vecinos
 
 def menor_vecino(vecinos,pesos):
@@ -51,8 +54,39 @@ def menor_vecino(vecinos,pesos):
             
     return vecino_menor,vecino_menor_p
 
-def tabu(distancias,lista_tabu,tenencia_tabu,criterio_aspiracion,camino,peso):    
-    vecinos(camino, lista_tabu)
+def diferencia (camino2, camino1,lista_tabu):
+    diff=[]
+    for i in range(len(camino1)):        
+        if camino1[i] != camino2[i]:
+            #diff.insert(0,camino2[i] )
+            diff.append(camino2[i])
+            #print(diff,"Aqui va diff"
+    if camino1[-1] !=camino2[-1]:
+        diff[0],diff[1] = diff[1],diff[0]
+    
+    if (len(lista_tabu) == 3):
+        lista_tabu.pop()
+    lista_tabu.insert(0,diff)        
+    #lista_tabu.append(diff)
+    
+    return lista_tabu
+
+def tabu(distancias,lista_tabu,tenencia_tabu,criterio_aspiracion,camino,peso):
+    for i in range(5):    
+        vecinos_tabu=vecinos(camino, lista_tabu)
+        camino_menor_p = menor_vecino(vecinos_tabu, distancias)
+        #print(camino," ",camino_menor_p[0])
+        lista_nueva=diferencia(camino, camino_menor_p[0], lista_tabu)
+        camino= camino_menor_p[0]
+        
+        
+        if(criterio_aspiracion > camino_menor_p[1]):
+            criterio_aspiracion = camino_menor_p[1]
+            camino_corto = camino
+    
+    
+    
+    return camino_corto,criterio_aspiracion
     
         
 pesos = [[0,2,3,4,1],[1,0,4,1,2],[2,3,0,2,3],[3,4,1,0,4],[4,1,2,3,0]]
@@ -69,7 +103,9 @@ camino=[1,2,3,4,5]
 #print(camino)
 #print(calcular_peso(camino, pesos))
 #print(vecinos(camino, lista_tabu))
-vecino_act=vecinos(camino, lista_tabu)
-print(menor_vecino(vecino_act, pesos))
+#vecino_act=vecinos(camino, lista_tabu)
+#print(menor_vecino(vecino_act, pesos))
 #tabu(pesos, lista_tabu, tenencia_tabu, peso_inicial, camino, peso_inicial)
+print(tabu(pesos,lista_tabu,tenencia_tabu,peso_inicial,camino,peso_inicial))
+print("tabu :",lista_tabu)
 
